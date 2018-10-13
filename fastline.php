2004d31bae50dfcacd91494de1feebdbbf918e38
Carl Li <leekunhui@sina.cn>
2018-10-13 17:56:43 +1300

    fastline

--- fastline.php
+++ fastline.php
@@ -24,36 +24,56 @@ var vm = new Vue({
     data: {
         stage: 1, // pointout which stage now
         terms: "This is the terms for Fast Speed.", // for shipment tab, item 2
-        deliverySpeed: "1"
+        deliverySpeed: "1",
+
+        // for progress
+        progressClass0: "progress-bar",
+        progressClass1: "progress-bar bg-dark",
+        progressClass2: "progress-bar bg-dark",
+        progressClass3: "progress-bar bg-dark",
+        progressClass4: "progress-bar bg-dark",
+        progressClass5: "progress-bar bg-dark"
+
         
     },
     methods: {
         // onclick Next
         nextHandler: function(){
+            
             switch(this.stage){
                 case 1:
                     $('#pills-origin-tab').tab('dispose')
                     $('#pills-package-tab').tab('show')
+                    this.progressClass1 = "progress-bar"
+                    scroll(0, 0)
                     this.stage += 1
                     break;
                 case 2:
                     $('#pills-package-tab').tab('show')
                     $('#pills-shipment-tab').tab('show')
+                    this.progressClass2 = "progress-bar"
+                    scroll(0, 0)
                     this.stage += 1
                     break;
                 case 3:
                     $('#pills-shipment-tab').tab('dispose')
                     $('#pills-rates-tab').tab('show')
+                    this.progressClass3 = "progress-bar"
+                    scroll(0, 0)
                     this.stage += 1
                     break;
                 case 4:
                     $('#pills-rates-tab').tab('dispose')
                     $('#pills-payment-tab').tab('show')
+                    this.progressClass4 = "progress-bar"
+                    scroll(0, 0)
                     this.stage += 1
                     break;
                 case 5:
                     $('#pills-payment-tab').tab('dispose')
                     $('#pills-notification-tab').tab('show')
+                    this.progressClass5 = "progress-bar"
+                    scroll(0, 0)
                     this.stage += 1
                     break;
                 case 6:
@@ -61,6 +81,7 @@ var vm = new Vue({
 
                     break;
             }
+
         },
         getDeliverySpeedTerm: function() {
             switch (this.deliverySpeed) {
