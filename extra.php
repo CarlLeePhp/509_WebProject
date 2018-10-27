bdf9cd355f52f85d26624183afcae6c33263c68f
Carl Li <leekunhui@sina.cn>
2018-10-27 21:20:37 +1300

    fix01

---
+++ extra.php
@@ -0,0 +1,19 @@
+<?php
+// This header.html include the content from DOCTYPE to the end of navigation.
+include './view/header.html';
+
+// Main
+include './view/extra.html';
+
+// This footer.html include footer and JS libs.
+include './view/footer.html';
+
+// JS for this page should be writen after here.
+?>
+
+
+
+
+</body>
+
+</html>
