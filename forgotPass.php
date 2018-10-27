1cd44c31aae6899ee4eb1d20c9f0583986e9d8da
Carl Li <leekunhui@sina.cn>
2018-10-27 22:53:28 +1300

    fix-02 	new file:   forgotPass.php 	modified:   view/custom.html 	new file:   view/forgotPass.html 	modified:   view/packing.html 	modified:   view/prohibited.html

---
+++ forgotPass.php
@@ -0,0 +1,33 @@
+<?php
+// This header.html include the content from DOCTYPE to the end of navigation.
+include './view/header.html';
+
+// Main
+include './view/forgotPass.html';
+
+// This footer.html include footer and JS libs.
+include './view/footer.html';
+
+// JS for this page should be writen after here.
+?>
+
+<script>
+    // for computer, erreohd
+    //document.getElementById("send1").addEventListener("click", function(event){
+    //   event.preventDefault();
+    //    document.getElementById('errorhd').hidden = false
+    //});
+
+    // for mobile, errorhm
+    //document.getElementById("send").addEventListener("click", function(event){
+    //    event.preventDefault();
+    //    document.getElementById('errorhm').hidden = false
+    //});
+
+
+</script>
+
+
+</body>
+
+</html>
